4fc93fdded5e9b9c54bc6e85de01c53e49446a2d
renato <renatonm9@gmail.com>
2025-04-29 22:17:21 -0300

    execicio php

--- 01/exercicio/recebecliente.php
+++ 01/exercicio/recebecliente.php
@@ -1,7 +1,17 @@
 <?php
 
-/*
+
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+$telefone = $_GET['telefone'];
+
+
+echo "nome: $nome <br> Email: $email <br> Telefone $telefone";
+
+
+?>
+<!-- /*
 Nome
 Email
 Telefone
-*?
+*? -->
