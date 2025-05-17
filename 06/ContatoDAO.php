ce251b1ee966b403f9167378935b97771f8001b3
RENATO NAVARRO MARQUES GONZAGA <renato.nmgonzaga@senacsp.edu.br>
2025-05-16 22:03:29 -0300

    excluir

--- 06/ContatoDAO.php
+++ 06/ContatoDAO.php
@@ -1,67 +1,69 @@
 <?php
 
-
 require_once 'Database.php';
 require_once 'Contato.php';
 
-class ContatoDAO{
+class ContatoDAO
+{
+    private $db; // usado em todas as funções
 
-    private $db; // usa em todas as funçoes
-
-    public function __construct(){
-        $this->db = Database::getInstance();// uma instancia do banco de dados
-        
+    public function __construct()
+    {
+        $this->db = Database::getInstance();        
     }
 
-
-    public function getAll(){
+    public function getAll()
+    {
         $stmt = $this->db->query("SELECT * FROM contatos");
-        $contatos = []; // inicia um array vazio
+        $contatos = []; // inicializa um array vazio
 
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            $contatos[] = new Contato($row['id'],$row['nome'],$row['telefone'],$row['email'],$row['endereco']);
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            $contatos[] = new Contato($row['id'], $row['nome'], $row['telefone'], $row['email'], $row['endereco']);
         }
+
         return $contatos;
     }
 
-    public function getById(int $id) : ?Contato{
-
-       
-        $stmt = $this->db->prepare("SELECT * FROM contatos WHERE id = :id");
+    public function getById(int $id): ?Contato
+    {
+        $stmt = $this->db->prepare("SELECT * FROM agenda.contatos WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
         return $row? new Contato(
             $row['id'], 
             $row['nome'], 
-            $row['telefone'],
-             $row['email'], 
-             $row['endereco'])
-             :null;
-
+            $row['telefone'], 
+            $row['email'], 
+            $row['endereco'])
+            : null;
     }
 
-    public function create(Contato $contato){
+    public function create(Contato $contato) 
+    {
+        $sql = "INSERT INTO contatos (nome, telefone, email, endereco) VALUES 
+	        (:nome, :telefone, :email, :endereco)";
+        $stmt = $this->db->prepare($sql);
 
-        $stmt = $this->db->prepare("INSERT INTO contatos (nome,telefone,email,endereco) VALUES 
-        (:nome, :telefone, :email, :endereco)");
+        $nome = $contato->getNome();
+        $telefone = $contato->getTelefone();
+        $email = $contato->getEmail();
+        $endereco = $contato->getEndereco();
 
-$nome = $contato->getNome();
-$telefone = $contato->getTelefone();
-$email = $contato->getEmail();
-$endereco = $contato->getEndereco();
-
-$stmt->bindParam(':nome', $nome);
-$stmt->bindParam(':telefone', $telefone);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':endereco', $endereco);
-$stmt->execute();
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':endereco', $endereco);
+        $stmt->execute();
     }
 
-
+    public function delete(int $id): void
+    {
+        $stmt = $this->db->prepare("DELETE FROM contatos WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
 }
-
-
-
 ?>
