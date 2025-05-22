8551f5928e2ab4ae00b90416e4a200aff4a52cdb
RENATO NAVARRO MARQUES GONZAGA <renato.nmgonzaga@senacsp.edu.br>
2025-05-22 19:21:51 -0300

    correção

--- 07/backend/produtoDAO.php
+++ 07/backend/produtoDAO.php
@@ -1,6 +1,16 @@
 <?php
 
+
+require_once "Produto.php";
+require_once "Database.php.php";
 class produtoDAO{
+    private $db;
+
+
+    public function __construct()
+    {
+        $this->db = produtoDAO::getInstance();
+    }
 
     public function getAll() : array{
 
@@ -15,6 +25,18 @@ class produtoDAO{
 
     public function create(Produto $produto): void{
 
+    }
+
+    public function createInseguro(Produto $produto): void
+    {
+        $sql = "INSERT INTO produtos (nome, preco, ativo, dataDeCadastro, dataDeValidade) VALUES
+                ({$produto->getNome()}, 
+                {$produto->getPreco()}, 
+                {$produto->getAtivo()},
+                {$produto->getDataDeCadastro()}, 
+                {$produto->getDataDeValidade()})";
+        
+        $this->db->query($sql);
     }
 
     public function update(Produto $produto): void{
